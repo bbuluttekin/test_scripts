2e5b14212396ed40c8be4e34985901e61cf2e586
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2018-07-27 17:24:07 +0100

    New chatacter method added and init function created.

--- scripst/OOP/characters.py
+++ scripst/OOP/characters.py
@@ -5,7 +5,15 @@ import random
 class Thief:
     sneaky = True
 
+    def __init__(self, name, sneaky=True, **kwargs):
+        self.name = name
+        self.sneaky = sneaky
+
+        for key, value in kwargs.items:
+            setattr(self, key, value)
+
     def pickpocket(self):
-        if self.sneaky:
-            return bool(random.randint(0, 1))
-        return False
+        return self.sneaky and bool(random.randint(0, 1))
+
+    def hide(self, light_level):
+        return self.sneaky and light_level < 10
