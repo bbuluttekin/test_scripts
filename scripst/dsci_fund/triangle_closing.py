351566ed003004878f0a6b1548b283e57646b1b7
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2018-09-01 21:30:50 +0100

    Encapsulate the code in functions

--- scripst/dsci_fund/triangle_closing.py
+++ scripst/dsci_fund/triangle_closing.py
@@ -1,4 +1,5 @@
 import csv
+import collections as coll
 
 # open and parse CSV file
 csvfile = open("../reviews.csv", newline='')
@@ -9,23 +10,37 @@ records = list(reader)
 jonathan = "1465258"
 jonathan_reviews = []
 
-# find listings of users
-for row in records:
-    if row[3] == jonathan:
-        jonathan_reviews.append(row)
 
- # find fellow travellers
-fellow_travellers = set()
+def find_listings(records, user_id):
+    # find listings of users
+    listings = set()
+    for row in records:
+        if row[3] == user_id:
+            listings.add(row[0])
+    return listings
 
-listings = set([review[0] for review in jonathan_reviews])
 
-for row in records:
-    if row[0] in listings:
-        fellow_travellers.add(row[3])
+def find_travelers(records, listings):
+     # find fellow travellers
+    fellow_travellers = set()
+    for row in records:
+        if row[0] in listings:
+            fellow_travellers.add(row[3])
+    return fellow_travellers
 
-fellow_listing = []
 
-# fins triangles user is a part of
-for row in records:
-    if row[3] in fellow_travellers:
-        fellow_listing.append(row[0])
+def count_triangles(records, fellow_travelers):
+    triangles = []
+    # fins triangles user is a part of
+    for row in records:
+        if row[3] in fellow_travelers:
+            triangles.append(row[0])
+    return coll.Counter(triangles)
+
+
+def recommend_listings(counts, user_listing):
+    for listing in user_listing:
+        if listing in counts:
+            counts.pop(listing)
+
+    return counts.most_common(5)
