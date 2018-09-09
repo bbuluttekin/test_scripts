0fcb5ade624ca5b3a08d59f3d2e3703a9d908fb9
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2018-09-09 23:12:36 +0100

    Expanded method explaination

--- scripst/dsci_fund/requests.py
+++ scripst/dsci_fund/requests.py
@@ -3,3 +3,5 @@ import requests
 res = requests.get("http://www.nytimes.com")
 
 print(res.headers)  # prints headers sent by server
+print(res.status)  # status code of requests
+print(res.ok)  # Boolean value to confirm status ok or not
