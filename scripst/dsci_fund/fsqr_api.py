ef9fafbe10f242fe3fac6bc1f1c4f731bce42d8d
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2018-09-12 22:58:24 +0100

    Fsqr api wrapped in fuction to use any given query

---
+++ scripst/dsci_fund/fsqr_api.py
@@ -0,0 +1,24 @@
+import requests
+import json
+
+
+def get_places(query):
+    url = 'https://api.foursquare.com/v2/venues/explore'
+    with open('config.json', 'r') as f:
+        file_str = f.read()
+        file_str = json.loads(file_str)
+        CLIENT_ID = file_str['CLIENT_ID']
+        CLIENT_SECRET = file_str['CLIENT_SECRET']
+
+    params = {
+        'client_id': CLIENT_ID,
+        'client_secret': CLIENT_SECRET,
+        'v': '20180323',
+        'll': '40.7243,-74.0018',
+        'query': query,
+    }
+    res = requests.get(url, params=params)
+    return res.json()
+
+
+print(get_places("coffee"))
