decabfcbe96a81548f514e0b3e6b2ecc231a1f6e
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2018-09-20 15:10:56 +0100

    lat lon option made modularized

--- scripst/dsci_fund/fsqr_api.py
+++ scripst/dsci_fund/fsqr_api.py
@@ -2,7 +2,7 @@ import requests
 import json
 
 
-def get_places(query):
+def get_places(query, ll='40.7243,-74.0018'):
     url = 'https://api.foursquare.com/v2/venues/explore'
     with open('config.json', 'r') as f:
         file_str = f.read()
@@ -14,7 +14,7 @@ def get_places(query):
         'client_id': CLIENT_ID,
         'client_secret': CLIENT_SECRET,
         'v': '20180323',
-        'll': '40.7243,-74.0018',
+        'll': ll,
         'query': query,
     }
     res = requests.get(url, params=params)
