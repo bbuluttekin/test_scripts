529900df2f86ee78b69547f8204a4e40c2df697f
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2018-10-04 12:38:23 +0100

    Section 19 to 28

--- scripst/pop1.py
+++ scripst/pop1.py
@@ -106,3 +106,115 @@ elif int_x < 0:
     print(-1)
 else:
     print(0)
+
+# Minimum of three numbers
+
+n_1 = int(input())
+n_2 = int(input())
+n_3 = int(input())
+if n_2 >= n_1 <= n_3:
+    print(n_1)
+elif n_1 >= n_2 <= n_3:
+    print(n_2)
+else:
+    print(n_3)
+
+# Leap year
+
+year = int(input())
+if (year % 4 == 0) and (year % 100 != 0):
+    print("LEAP")
+elif year % 400 == 0:
+    print("LEAP")
+else:
+    print("COMMON")
+
+# Equal numbers
+
+a = int(input())
+b = int(input())
+c = int(input())
+if a == b == c:
+    print(3)
+elif (a != b) and (b != c) and (a != c):
+    print(0)
+else:
+    print(2)
+
+# Rook move
+
+st_1 = int(input())
+st_2 = int(input())
+mv_1 = int(input())
+mv_2 = int(input())
+if (st_1 == mv_1) or (st_2 == mv_2):
+    print('YES')
+else:
+    print('NO')
+
+# Chess board
+
+st_1 = int(input())
+st_2 = int(input())
+mv_1 = int(input())
+mv_2 = int(input())
+if (st_1 + st_2 + mv_1 + mv_2) % 2 == 0:
+    print('YES')
+else:
+    print('NO')
+
+# King move
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if abs(x1 - x2) <= 1 and abs(y1 - y2) <= 1:
+    print('YES')
+else:
+    print('NO')
+
+# Bishop moves
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if abs(x1 - x2) == abs(y1 - y2):
+    print('YES')
+else:
+    print('NO')
+
+# Queen move
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+if abs(x1 - x2) == abs(y1 - y2) or x1 == x2 or y1 == y2:
+    print('YES')
+else:
+    print('NO')
+
+# Knight move
+
+x1 = int(input())
+y1 = int(input())
+x2 = int(input())
+y2 = int(input())
+dx = abs(x1 - x2)
+dy = abs(y1 - y2)
+if dx == 1 and dy == 2 or dx == 2 and dy == 1:
+    print('YES')
+else:
+    print('NO')
+
+# Chocolate bar
+
+n = int(input())
+m = int(input())
+k = int(input())
+if k < n * m and ((k % n == 0) or (k % m == 0)):
+    print('YES')
+else:
+    print('NO')
